99286c734d4378357f94efffb1022e0281c7e79d
karlminator <kf.holmsten@gmail.com>
2025-04-08 14:24:02 +0200

    added methods and corresponding tests

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,4 +5,16 @@ public class Calculator {
     public int add(int x, int y) {
         return x + y;
     }
+
+    public int sub(int x, int y) {
+        return x - y;
+    }
+
+    public int mult(int x, int y) {
+        return x * y;
+    }
+
+    public int div(int x, int y) {
+        return x / y;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,37 @@
+import org.example.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    private Calculator calc;
+
+    @BeforeEach
+    void setupCalc(){
+        calc = new Calculator();
+    }
+
+    @Test
+    void testAdd() {
+        int res = calc.add(5,6);
+        assertEquals(11, res, "add works");
+
+    }
+
+    @Test
+    void testSub(){
+        assertEquals(0, calc.sub(5,5));
+    }
+
+    @Test
+    void testMult(){
+        assertEquals(25, calc.mult(5,5));
+    }
+
+    @Test
+    void testDiv(){
+        assertEquals(1, calc.div(5,5));
+    }
+
 }
